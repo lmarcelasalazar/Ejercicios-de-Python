56ac9effb838e136076a0bec99373443ef571692
Laura <salazar.lauramarcela31@gmail.com>
2021-03-20 17:26:50 -0500

    ciclosAnidados
    ejercicios con ciclos anidados

---
+++ ciclosAnidados.py
@@ -0,0 +1,73 @@
+#cont=0
+#while(cont<5):
+  #  nombre=input("Ingrese su nombre: ")
+  #  cont1=0
+   # promedio=0
+   # cont_gan=0
+   # cont_per=0
+   # suma=0
+   # while(cont1<5):
+   #     nota_materia=float(input("Ingrese la nota: "))
+   #     suma=suma+nota_materia
+   #     print(cont1)
+   #     cont1=cont1+1
+   # promedio=suma/5
+   # print("El promedio es: ",promedio)
+
+   # if(promedio>3.0):
+    #    print("Gano la materia")
+     #   cont_gan=cont_gan+1
+    #else:
+     #   cont_per=cont_per+1
+      #  print("No gano la materia!")
+
+
+    #cont=cont1+1
+#print("estudiantes que ganaron: ",cont_gan)
+#print("estudiantes que perdieron: ",cont_per)
+
+
+
+#cont=0
+#while(cont<5):
+ #   nombre_equipo=input("Ingrese el nombre del equipo: ")
+  #  cont1=0
+   # nombre_jugador=0
+    #posicion=0
+    #while(cont1<2):
+     #  documento=int(input("Ingrese su documento: "))
+      #  nombre_jugador=input("Ingrese su nombre: ")
+       # posicion=input("Ingrese su posicion: ")
+        #cont1=cont1+1
+
+    #cont=cont1+1
+    #print("Nombre del equipo: ",nombre_equipo)
+    #print("Nombre del jugador: ",nombre_jugador)
+    #print("Posicion: ",posicion)
+
+
+
+cont=0
+edad=0
+altura=0
+peso=0
+imc=0
+while(cont<2):
+    edad=int(input("Ingrese su edad: "))
+    peso=int(input("Ingrese su peso: "))
+    altura=float(input("Ingrese su estatura: "))
+    cont1=0
+while(cont1<3):
+    cont1=cont1+1
+    imc=peso*altura
+    
+print("su imc es de: ",imc)
+    
+    
+
+    
+    
+
+
+
+
